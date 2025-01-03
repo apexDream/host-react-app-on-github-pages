508e0e20fe5ee520fa37230d46a74d2cce9c35fc
Eli Caleb Anderson <caleb@ecaent.com>
2025-01-03 13:00:11 -0600

    test 4

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,15 +1,13 @@
-import Image from 'next/image';
-
 const Avatar = () => {
   return (
     <div className="hidden xl:flex xl:max-w-none">
-      <Image 
-        src={`${process.env.NEXT_PUBLIC_BASE_PATH || ''}/WithColor.png`} 
-        width={737} 
-        height={678} 
-        alt='' 
-        className='translate-z-0 w-full h-full mix-blend-hard-light' 
-        />
+      <img
+        src="/images/WithColor.png" 
+        alt="With Color"
+        width="737"
+        height="678"
+        className="translate-z-0 w-full h-full mix-blend-hard-light"
+      />
     </div>
   );
 };
